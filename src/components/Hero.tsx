b7e8baf94648acfe8a04579c909becc33940f827
prathmesh <prathmeshpol61@gmail.com>
2024-02-05 17:38:49 +0530

    styles changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,19 +5,25 @@ import heroImg from "@/../public/asset/Coding-bro.png";
 
 const Hero = () => {
   return (
-    <div className="relative  h-full max-w-[1340px] m-auto flex flex-wrap justify-between items-center gap-10 mt-10 lg:mt-0">
+    <div className="relative h-full max-w-[1340px] m-auto flex flex-wrap justify-between items-center gap-10 mt-10 lg:mt-0">
       <div className="flex items-start flex-col gap-y-4 max-w-[600px]">
         <h1 className=" lg:text-5xl text-4xl text-white">
           Hello,
           <br /> <b>my name's Prathmesh.</b>
         </h1>
         <p className="lg:text-xl text-base text-white">
-          Experienced in building{" "}
-          <b className="border-b-2 border-purple-400">full stack</b> web
+          Experienced in building
+          <b className="border-b-2 border-purple-400"> full stack</b> web
           applications,Website UI's and Web Designes
         </p>
       </div>
-      <Image alt="heroImg" src={heroImg} width={700} height={300} />
+      <Image
+        alt="heroImg"
+        src={heroImg}
+        width={700}
+        height={300}
+        blurDataURL="Hero Image"
+      />
 
       <div className="react absolute xl:block hidden  top-[20%]  w-[80px] h-[40px]">
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
@@ -29,7 +35,7 @@ const Hero = () => {
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
       </div>
 
-      <div className="typescript absolute xl:block hidden bottom-[25%] left-[20%] w-[80px] h-[40px]">
+      <div className="typescript absolute 2xl:block  hidden bottom-[25%] left-[20%] w-[80px] h-[40px]">
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
       </div>
     </div>
--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { social } from "../../utils";
 import Link from "next/link";
 
-type Props = {};
-
-const SocialMedia = (props: Props) => {
+const SocialMedia = () => {
   return (
-    <section className="xl:fixed static top-[50%] left-0">
-      <div className="flex xl:flex-col items-center justify-center bg-[#740FF5] p-4 xl:h-fit xl:w-fit gap-4 w-full h-full ">
+    <section className="2xl:fixed static top-[50%] left-0">
+      <div className="flex 2xl:flex-col items-center justify-center bg-[#740FF5] p-4 2xl:h-fit 2xl:w-fit gap-4 w-full h-full ">
         {social.map((item) => (
-          <Link href={item.link} target="_blank">
+          <Link href={item.link} target="_blank" key={item.link}>
             <item.icon />
           </Link>
         ))}
